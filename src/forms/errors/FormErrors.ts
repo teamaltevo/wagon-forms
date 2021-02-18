bdcb3f7a9f0111813687ea07f3eed2cb503144bc
Olivier Brassard <brassard.oli@gmail.com>
2021-02-18 00:17:03 -0500

    Add support for Mirror Fields

--- src/forms/errors/FormErrors.ts
+++ src/forms/errors/FormErrors.ts
@@ -1,6 +1,7 @@
 export enum FormErrors {
-	INVALID_EMAIL_ADDRESS = 'forms.errors.invalidEmailAddress',
-	REQUIRED_FIELD = 'forms.errors.requiredField',
-	MAX_LENGTH_OVERFLOW = 'forms.errors.maxLength',
-	MIN_LENGTH_REQUIRED = 'forms.errors.minLength'
+    INVALID_EMAIL_ADDRESS = 'forms.errors.invalidEmailAddress',
+    REQUIRED_FIELD = 'forms.errors.requiredField',
+    MAX_LENGTH_OVERFLOW = 'forms.errors.maxLength',
+    MIN_LENGTH_REQUIRED = 'forms.errors.minLength',
+    FIELDS_DOESNT_MATCH = "forms.errors.fields_doesnt_match"
 }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { FieldViewModel } from './forms/viewmodels/fields/FieldViewModel';
 export { EmailFieldViewModel } from './forms/viewmodels/fields/EmailFieldViewModel';
 export { PasswordFieldViewModel } from './forms/viewmodels/fields/PasswordFieldViewModel';
 export { TextFieldViewModel } from './forms/viewmodels/fields/TextFieldViewModel';
+export { MirrorFieldViewModel, MirrorFieldInitializer } from './forms/viewmodels/fields/MirrorFieldViewModel';
 
 // Forms view models
 export { FormViewModel } from './forms/viewmodels/FormViewModel';
@@ -12,4 +13,4 @@ export { FormViewModel } from './forms/viewmodels/FormViewModel';
 // Validation
 export { ValidationResult } from './forms/viewmodels/ValidationResult';
 export { ValidatableInput, ValidationCallback } from './forms/viewmodels/ValidatableInput';
-export { CustomValidator } from './forms/viewmodels/fields/FieldViewModelInitializer';
+export { CustomValidator, FieldViewModelInitializer } from './forms/viewmodels/fields/FieldViewModelInitializer';
